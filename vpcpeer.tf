b352599c7ff2f5ad3562d9ea5668b0d42750adee
atul <apatel@aviatrix.com>
2024-04-28 15:02:55 +0100

    update vpcpeer - alicn

--- vpcpeer.tf
+++ vpcpeer.tf
@@ -57,24 +57,6 @@ output "route_table_ids" {
 }
 
 
-resource "alicloud_route_entry" "toaliglobal" {
-  provider = alicloud.china
-  route_table_id        = "${alicloud_vpc.accepting_vpc.router_table_id}"
-  destination_cidrblock = var.ali_global_cidr
-  nexthop_type          = "VpcPeer"
-  nexthop_id            = "${alicloud_vpc_peer_connection.default.id}"
-}
-
-
-resource "alicloud_route_entry" "alicn" {
-  provider = alicloud.global                                 # needed provider ; without didn't find RT
-  route_table_id        =  "${data.alicloud_route_tables.aliglobal.tables[0].route_table_id}"
-  destination_cidrblock = var.ali_cn_cidr
-  nexthop_type          = "VpcPeer"
-  nexthop_id            = "${alicloud_vpc_peer_connection.default.id}"
-}
-
-
 # ACCEPTOR
 #  get acceptor vpc route table id
 data "alicloud_route_tables" "alicn" {
@@ -91,6 +73,22 @@ output "alicn_route_table_ids" {
   value = "${data.alicloud_route_tables.alicn.ids}"
 }
 
+resource "alicloud_route_entry" "toaliglobal" {
+  provider = alicloud.china
+  route_table_id        = "${data.alicloud_route_tables.alicn.tables[0].route_table_id}"
+  destination_cidrblock = var.ali_global_cidr
+  nexthop_type          = "VpcPeer"
+  nexthop_id            = "${alicloud_vpc_peer_connection.default.id}"
+}
+
+
+resource "alicloud_route_entry" "alicn" {
+  provider = alicloud.global                                 # needed provider ; without didn't find RT
+  route_table_id        =  "${data.alicloud_route_tables.aliglobal.tables[0].route_table_id}"
+  destination_cidrblock = var.ali_cn_cidr
+  nexthop_type          = "VpcPeer"
+  nexthop_id            = "${alicloud_vpc_peer_connection.default.id}"
+}
 
 
 
